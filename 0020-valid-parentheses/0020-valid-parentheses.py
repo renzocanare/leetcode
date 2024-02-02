5c87c9fe8e59893a173f205fae93efa4a5765c77
Renzo Canare <69526313+renzocanare@users.noreply.github.com>
2024-02-02 17:34:31 +0800

    Time: 39 ms (44.77%), Space: 16.6 MB (70.33%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -5,27 +5,30 @@ class Solution:
         # Use a stack to LIFO to ensure the opening is properly closed.
         stack = []
         
-        bracketMap = {
-            '}': '{',
-            ']': '[',
-            ')': '('
-        }
-        openBracket = ['(', '[', '{']
-        closeBracket = [')', ']', '}']
+        openBracket = '([{'
+        closeBracket = ')]}'
         
         for char in s:
             if char in openBracket:
+                # If opening character, push to stack.
                 stack.append(char)
-            
-            if char in closeBracket:
+            elif char in closeBracket:
+                # If closing character, pop stack and check if its the correspoding opening pair.
                 if not stack:
                     return False
                 
-                top = stack[-1] # Peek into stack.
+                # Peek into stack.
+                top = stack[-1]
                 
-                if top == bracketMap[char]:
+                # If the top of the stack is the corresponding opening pair, its correct so pop and continue.
+                if top == openBracket[closeBracket.find(char)]:
                     stack.pop()
                 else:
+                    # If not, return False.
                     return False
+            else:
+                pass
+            
+        # If stack empty after all iterations, means correct so return True.
         if not stack:
             return True
