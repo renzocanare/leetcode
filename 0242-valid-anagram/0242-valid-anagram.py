60f9f18648c635cfc0b884ec473b885cf81080a9
Renzo Canare <69526313+renzocanare@users.noreply.github.com>
2024-02-06 18:28:43 +0800

    Time: 67 ms (12.74%), Space: 17.3 MB (57.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,10 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
+    def isAnagram(self, s: str, t: str) -> bool: 
         if len(s) != len(t):
+            # If the lengths of the text are not equal, it cannot be an anagram.
             return False
-        
-        if ''.join(sorted(s)) == ''.join(sorted(t)):
+        elif ''.join(sorted(s)) == ''.join(sorted(t)):
+            # If the alphabetically sorted strings are equal, it means they can be an anagram of each other.
             return True
+        else:
+            return False
