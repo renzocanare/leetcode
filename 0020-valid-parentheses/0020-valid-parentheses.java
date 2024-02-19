409abc6ec3f1335d7af68d1fd21a34231f4a5463
Renzo Canare <69526313+renzocanare@users.noreply.github.com>
2024-02-19 22:26:57 +0800

    Time: 17 ms (6.41%), Space: 42.2 MB (5.72%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,43 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+
+class Solution {
+    
+    // Helper function to check for matching pairs.
+    private boolean isMatching(char open, char close) {
+        return (open == '(' && close == ')') ||
+            (open == '{' && close == '}') ||
+            (open == '[' && close == ']');
+    }
+    
+    public boolean isValid(String s) {
+        // Initialize a stack.
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        // Return false for single char strings.
+        if ((s.length() == 1)) {
+            return false;
+        } 
+            
+        for (int i = 0; i < s.length(); i++) {
+            // Get first character.
+            char currChar = s.charAt(i);
+            System.out.println(currChar);
+            if ( currChar == ')' || currChar == '}' || currChar == ']') {
+                if (stack.isEmpty() || !isMatching(stack.peekLast(), currChar)){
+                    // If stack is empty or is not a matching pair, return false.
+                    return false;
+                } else {
+                    // Pop it and continue.
+                    stack.removeLast();
+                }
+            } else {
+                // Push to stack if opening bracket.
+                stack.add(currChar);
+            }
+        }
+        // Return if stack is empty or not; all matched should be empty, otherwise it will not.
+        return stack.isEmpty();
+    }
+}
