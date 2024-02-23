c2105490cd786bd7e13c9ead9c13131d8e7be087
Renzo Canare <69526313+renzocanare@users.noreply.github.com>
2024-02-23 16:19:09 +0800

    Time: 63 ms (41.12%), Space: 16.7 MB (90.78%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,17 +1,23 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        letterMap = {}
+        letterMap = {}  # Map to store letter counts.
+        
+        # Store character counts into letterMap.
         for char in magazine:
             if char in letterMap:
                 letterMap[char] += 1
             else:
                 letterMap[char] = 1
         
+        # Check if can rebuild ransomNote using letterMap.
         for char in ransomNote:
             if char not in letterMap or letterMap[char] <= 0:
+                # If there are missing characters or that particular character has no more usable counts,
+                # return False.
                 return False
             else:
                 letterMap[char] -= 1
         
+        # If can successfully rebuild string, return True.
         return True
         
